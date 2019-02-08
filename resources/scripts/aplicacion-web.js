557dfdd9e434ba91d728744558d160b76ecf379e
IrvingMg <mirvingr@gmail.com>
2019-02-07 22:02:51 -0600

    Layout de inicio con MDC

--- resources/scripts/aplicacion-web.js
+++ resources/scripts/aplicacion-web.js
@@ -1,3 +1,16 @@
 import {MDCRipple} from '@material/ripple';
+import {MDCSelect} from '@material/select';
+import {MDCTopAppBar} from '@material/top-app-bar/index';
+import {MDCChipSet} from '@material/chips';
+const chipSetEl = document.querySelector('.mdc-chip-set');
+const chipSet = new MDCChipSet(chipSetEl);
 
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+const select = new MDCSelect(document.querySelector('.mdc-select'));
+
+const selector = '.mdc-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
