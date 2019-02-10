6a9d3f0e4c1e1c7b3d7c0abc5ebc34811a0ddaf3
IrvingMg <mirvingr@gmail.com>
2019-02-09 23:43:06 -0600

    Layout inicio de sesión y registrarse

--- resources/scripts/aplicacion-web.js
+++ resources/scripts/aplicacion-web.js
@@ -2,16 +2,28 @@ import {MDCRipple} from '@material/ripple';
 import {MDCTopAppBar} from '@material/top-app-bar/index';
 import {MDCChipSet} from '@material/chips';
 import {MDCSelect} from '@material/select';
+import {MDCTextField} from '@material/textfield';
 
+//Instancias comunes
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
-
-const chipSetEl = document.querySelector('.mdc-chip-set');
-const chipSet = new MDCChipSet(chipSetEl);
-
-const select = new MDCSelect(document.querySelector('.mdc-select'));
-
 const selector = '.mdc-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {  
+    return new MDCRipple(el);
 });
+
+//Instancias de páginas especificas
+if(document.querySelector('.mdc-chip-set')){
+    const chipSetEl = document.querySelector('.mdc-chip-set');
+    const chipSet = new MDCChipSet(chipSetEl);
+}
+
+if(document.querySelector('.mdc-select')){
+    const select = new MDCSelect(document.querySelector('.mdc-select'));
+}
+
+if (document.querySelector('.mdc-text-field')){
+    const textFields = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
+        return new MDCTextField(el);
+    });
+}
