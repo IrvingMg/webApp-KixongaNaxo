dda3c154d6d04b9c50ed0d6738e9899f1e7c2eb6
IrvingMg <mirvingr@gmail.com>
2019-02-11 01:50:00 -0600

    Integración de MDC en todas las páginas

--- resources/scripts/aplicacion-web.js
+++ resources/scripts/aplicacion-web.js
@@ -3,6 +3,9 @@ import {MDCTopAppBar} from '@material/top-app-bar/index';
 import {MDCChipSet} from '@material/chips';
 import {MDCSelect} from '@material/select';
 import {MDCTextField} from '@material/textfield';
+import {MDCTab} from '@material/tab';
+import {MDCFormField} from '@material/form-field';
+import {MDCRadio} from '@material/radio';
 
 //Instancias comunes
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
@@ -26,4 +29,16 @@ if (document.querySelector('.mdc-text-field')){
     const textFields = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
         return new MDCTextField(el);
     });
+}
+
+if (document.querySelector('.mdc-tab')){
+    const tabs = [].map.call(document.querySelectorAll('.mdc-tab'), function(el) {
+        return new MDCTab(el);
+    });
+}
+
+if (document.querySelector('.mdc-radio')){
+    const radio = new MDCRadio(document.querySelector('.mdc-radio'));
+    const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+    formField.input = radio;
 }
